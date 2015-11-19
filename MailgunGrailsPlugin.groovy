f703ced2c0d79252e802d5d3513b7751e39087f7
Orko <orquito@gmail.com>
2015-11-19 20:27:22 -0300

    develop: Change plugin version to 1.0.0

--- MailgunGrailsPlugin.groovy
+++ MailgunGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class MailgunGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "1.0.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.5 > *"
     // resources that are excluded from plugin packaging
