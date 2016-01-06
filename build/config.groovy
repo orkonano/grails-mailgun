db1d47dbd6a008929b35e282ed66934ff285da8c
Orko <orquito@gmail.com>
2016-01-06 06:46:52 -0300

    migrate-grails3: Remove invalid folder

--- build/config.groovy
+++
@@ -1,8 +0,0 @@
-
-withConfig(configuration) {
-    inline(phase: 'CONVERSION') { source, context, classNode ->
-        classNode.putNodeMetaData('projectVersion', '2.0.0-SNAPSHOT')
-        classNode.putNodeMetaData('projectName', 'grails-mailgun')
-        classNode.putNodeMetaData('isPlugin', 'true')
-    }
-}
